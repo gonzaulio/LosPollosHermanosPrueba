130e8879d920d2def2f45ef49f30453b824a3642
Julio González <gonzaulio@gmail.com>
2022-02-20 15:58:02 +0100

    subida

--- web-polloshermanos/src/app/components/carousel/carousel.component.ts
+++ web-polloshermanos/src/app/components/carousel/carousel.component.ts
@@ -10,13 +10,12 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 export class CarouselComponent implements OnInit {
 
   public imagenes:any[]=[
-    {img: 'assets/prueba/header-01.jpg', nombre: 'prueba1'},
-    {img: 'assets/prueba/header-05.jpg', nombre: 'prueba2'},
-    {img: 'assets/prueba/header-07.jpg', nombre: 'prueba3'},
+    {img: 'assets/files/header-01.jpg', nombre: 'files1'},
+    {img: 'assets/files/header-05.jpg', nombre: 'files2'},
+    {img: 'assets/files/header-07.jpg', nombre: 'files3'},
   ]
 
   constructor(config: NgbCarouselConfig) {
-    // customize default values of carousels used by this component tree
     config.interval = 5000;
     config.wrap = true;
   }
